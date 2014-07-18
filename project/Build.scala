c2f0c5ceb1d152d546d098bb03780144c3b99e58
Kuchitama <kiyo.kunihira@gmail.com>
2014-07-18 18:35:07 +0900

    edit sbt settings.

---
+++ project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+import scalariform.formatter.preferences._
+import com.typesafe.sbt.SbtScalariform._
+
+object BuildSettings {
+  val buildName = "EMLauncher4S"
+  val buildOrganization = "com.github.kuchitama"
+  val buildVersion = "1.0-SNAPSHOT"
+  val buildScalaVersion = "2.11.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    name := buildName,
+    organization := buildOrganization,
+    version := buildVersion,
+    scalaVersion := buildScalaVersion
+  )
+}
+
+object EMLauncher4SBuild extends Build {
+  import BuildSettings._
+  
+  
+  lazy val main = Project (
+    id = "emlaunhcer4s",
+    base = file(".")
+  ).settings(
+    libraryDependencies ++= Seq(
+      "net.databinder.dispatch" %% "dispatch-core" % "0.11.0",
+      "org.scalatest" %% "scalatest" % "2.0" % "test"
+    )
+  ).settings(
+    scalariformSettings : _*
+  ).settings(ScalariformKeys.preferences := FormattingPreferences()
+      .setPreference(IndentWithTabs, false)
+      .setPreference(DoubleIndentClassDeclaration, true)
+      .setPreference(PreserveDanglingCloseParenthesis, true)
+  )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,20 @@
+import sbt._
+import Defaults._
+
+// Comment to get more information during initialization
+logLevel := Level.Info
+
+ivyLoggingLevel := UpdateLogging.Full
+
+resolvers ++= Seq(
+    DefaultMavenRepository,
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+    Classpaths.typesafeReleases
+)
+
+// Use the Play sbt plugin for Play projects
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1")
+
